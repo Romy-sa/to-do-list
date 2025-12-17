f637f54aa096dc780a0881a06b8ae5fb250dc8cb
Romy-sa <106144793+Romy-sa@users.noreply.github.com>
2025-12-17 10:27:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const item = document.getElementById("todo-item");
+const date = document.getElementById("todo-date");
+const addBtn = document.getElementById("add-btn");
+const listItems = document.getElementById("list-items");
+const clearBtn = document.getElementById("clear-btn");
+
+
+let list = JSON.parse(localStorage.getItem("list"))  || [];
+
+renderListHTML();
+
+
+//    Set to-do list item
+addBtn.addEventListener("click", () => {
+  if(item.value ==! '' && date.value ==! '') {
+    let itemValue = item.value;
+    let dateValue = date.value;
+    list.push({itemValue,dateValue});
+
+    localStorage.setItem("list",JSON.stringify(list));
+
+    renderListHTML();
+
+    item.value = ``;
+    date.value = ``;
+  }
+});
+
+//    Clear the entire list
+clearBtn.addEventListener("click",() => {
+  list = [];
+  localStorage.removeItem("list");
+  listItems.innerHTML = ``;
+});
+
+
+//    Render the list itmes into the page
+function renderListHTML() {
+  let todoListHTML = ``;
+  list.forEach(element => {
+    let html = `
+    <div>
+      ${element.itemValue}
+    </div>
+    <div>
+      ${element.dateValue}
+    </div>
+    <button id="delete-btn" class="delete-btn">
+        Delete
+    </button>`;
+    todoListHTML += html;
+  });
+  listItems.innerHTML = todoListHTML;
+
+  document.querySelectorAll(".delete-btn").forEach((e,index) => {
+      e.addEventListener("click", () => {
+      list.splice(index,1);
+      renderListHTML();
+    });
+  })
+}
